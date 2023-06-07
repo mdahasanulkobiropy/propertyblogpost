24aa7fcaf43cc531c7261cfd26b4cef4e5e514ea
mdahasanulkobiropy <mdahasanulkobiropy@gmail.com>
2023-06-07 22:23:16 +0600

    register modal

--- resources/views/frontend/includes/register.blade.php
+++ resources/views/frontend/includes/register.blade.php
@@ -9,6 +9,7 @@
             <div class="modal-body">
                 <h5 class="modal-title" id="pxpSignupModal">Create an account</h5>
                 <form method="POST" action="{{ route('register') }}" class="mt-4">
+                    @csrf
                     <div class="form-group">
                         <label for="pxp-signup-firstname">Name</label>
                         <input type="text" class="form-control" name="name" id="pxp-signup-firstname" placeholder="Enter  name">
@@ -19,14 +20,14 @@
                     </div>
                     <div class="form-group">
                         <label for="pxp-signup-pass">Password</label>
-                        <input type="password" class="form-control"  id="pxp-signup-pass" placeholder="Create a password">
+                        <input type="password" class="form-control" name="password" id="pxp-signup-pass" placeholder="Create a password">
                     </div>
                     <div class="form-group">
                         <label for="pxp-signup-pass">Confirm Password</label>
                         <input type="password" class="form-control" name="password_confirmation" id="pxp-signup-pass" placeholder="Re-type password">
                     </div>
                     <div class="form-group">
-                        <a href="#" class="pxp-agent-contact-modal-btn">Sign Up</a>
+                        <button type="submit" class="pxp-agent-contact-modal-btn">Sign Up</button>
                     </div>
                     <div class="text-center mt-4 pxp-modal-small">
                         Already have an account? <a href="javascript:void(0);" class="pxp-modal-link pxp-signin-trigger">Sign in</a>
